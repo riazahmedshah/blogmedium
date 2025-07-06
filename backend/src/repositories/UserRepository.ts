ac80ca354a9eeb4232300bc13888cec355c92ace
riazahmedshah <riyazsh360@gmail.com>
2025-07-06 23:00:26 +0530

    added UserRepository

---
+++ backend/src/repositories/UserRepository.ts
@@ -0,0 +1,49 @@
+import { Prisma } from "@prisma/client";
+import { ExtendedPrismaClient } from "../config/db";
+
+
+export async function createUser(
+    prisma: ExtendedPrismaClient,
+    createUserInput: Prisma.UserCreateInput
+){
+    return await prisma.user.create({
+        data: createUserInput
+    });
+}
+
+export async function getUserById(
+    prisma: ExtendedPrismaClient,
+    id:string
+){
+    return await prisma.user.findFirst({
+        where: {
+            id
+        }
+    });
+}
+
+export async function updateUser(
+    prisma: ExtendedPrismaClient,
+    updateUserInput: Prisma.UserUpdateInput
+){
+    return await prisma.user.update({
+        where: {
+            id:updateUserInput.id
+        },
+        data:{
+            name:updateUserInput.name,
+            role:updateUserInput.role
+        }
+    });
+}
+
+export async function deleteUser(
+    prisma: ExtendedPrismaClient,
+    id:string
+){
+    return await prisma.user.delete({
+        where: {
+            id
+        }
+    });
+}
