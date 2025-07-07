8c63090d1ed5a587077a62c93161b0caf9720a95
riazahmedshah <riyazsh360@gmail.com>
2025-07-07 21:42:09 +0530

    user schema updated

--- backend/src/schemas/userSchema.ts
+++ backend/src/schemas/userSchema.ts
@@ -18,8 +18,17 @@ export const SigninSchema = z.object({
 export type SigninInput = z.infer<typeof SigninSchema>
 
 export const UpdateUserSchema = z.object({
-    name:z.string().min(2, "Name must be atleast 2 characters").max(20, "Name cannot be more that 20 characters"),
-    role:z.string()
+    name:z.string().min(2, "Name must be atleast 2 characters").max(20, "Name cannot be more that 20 characters").optional(),
+    role:z.string().optional()
+});
+
+export const userProfilePhotoSchema = z.object({
+    profilePhoto: z.object({
+        mimetype: z.string().refine((mimetype) => {
+            return ["image/jpeg", "image/png"].includes(mimetype)
+        },"PICTURE_MUST_BE_JPG_JPEG_PNG"),
+        size: z.number().max(5 * 1024 * 1024, "LIMIT_FILE_SIZE")
+    })
 });
 
 export type UpdateUserInput = z.infer<typeof UpdateUserSchema> 
