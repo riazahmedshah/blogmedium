b15f9b2eef2293559eee619e651f87fa9ed4b6a2
riazahmedshah <riyazsh360@gmail.com>
2025-07-07 11:10:27 +0530

    completed signup/signin

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,5 +1,8 @@
 import { Context } from "hono";
-import { UserSchema } from "../schemas/userSchema";
+import bcrypt from "bcrypt"
+import {sign} from "hono/jwt"
+
+import { SigninSchema, UserSchema } from "../schemas/userSchema";
 import { ResponseHandler } from "../utils/ResponseHandler";
 import { createUser, getUserByEmail } from "../repositories/UserRepository";
 import { createPrismaClient } from "../config/db";
@@ -15,7 +18,8 @@ export const CreateUser = async(c: Context) => {
         const prisma = createPrismaClient(c.env?.DATABASE_URL)
         const isUserExist = await getUserByEmail(prisma,data.email);
         if(!isUserExist){
-            const newUser = await createUser(prisma,body);
+            const hashPassword = await bcrypt.hash(data.password,10);
+            const newUser = await createUser(prisma, {...data, password:hashPassword});
             return ResponseHandler.created(c,{newUser})
         } else{
             return ResponseHandler.json(c,{
@@ -27,4 +31,38 @@ export const CreateUser = async(c: Context) => {
         return ResponseHandler.error(c, error);
     }
     
+}
+
+export const login = async (c:Context) => {
+    const body = await c.req.json();
+    
+    const {success, data, error} = SigninSchema.safeParse(body);
+    if(!success){
+        return ResponseHandler.zodError(c,error.errors);
+    }
+    try {
+        const prisma = createPrismaClient(c.env?.DATABASE_URL);
+        const user = await getUserByEmail(prisma, data.email);
+        if(user){
+            const isPassMatch = await bcrypt.compare(data.password, user.password);
+            if(!isPassMatch) return ResponseHandler.json(c,{
+                message:"INVALID_PASSWORD"
+            },401);
+
+            const token = await sign({
+                id:user.id,
+                exp: Math.floor(Date.now()/1000) + 60 * 5
+            },"secret",);
+
+            return ResponseHandler.json(c,{token})
+
+        } else{
+            return ResponseHandler.json(c,{
+                message:"USER_NOT_FOUND"
+            },404)
+        }
+    } catch (error) {
+        console.error(error)
+        return ResponseHandler.error(c,error)
+    }
 }
