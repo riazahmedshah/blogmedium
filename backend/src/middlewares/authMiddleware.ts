1c453f7d049c111094bbcff2b148edf512dd490d
riazahmedshah <riyazsh360@gmail.com>
2025-07-13 22:59:47 +0530

    added protected routes

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -10,8 +10,9 @@ export const authMiddleware = async (c: Context, next: Next) => {
     
     try {
         const decode = await verify(token, "secret");
-        if (decode && typeof decode.id === "number") {
+        if (decode) {
             c.set("userId", decode.id);
+            c.set("tokenId", decode.token)
             return await next();
         }
         return c.json({ message: "Invalid token payload" }, 401);
